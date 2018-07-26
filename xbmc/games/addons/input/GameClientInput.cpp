f67bd203cbcc4ae7c0ffa55c3fc9c082814180a4
Garrett Brown <themagnificentmrb@gmail.com>
2018-07-26 11:53:30 -0700

    Game add-ons: Factor out controller selection
    Previously, the first compatible controller for a given port was
    opened. Now, we mark the first compatible controller as active, then
    open the active port.

--- xbmc/games/addons/input/GameClientInput.cpp
+++ xbmc/games/addons/input/GameClientInput.cpp
@@ -60,6 +60,8 @@ CGameClientInput::~CGameClientInput()
 void CGameClientInput::Initialize()
 {
   LoadTopology();
+
+  ActivateControllers(m_controllers);
 }
 
 void CGameClientInput::Start()
@@ -74,7 +76,7 @@ void CGameClientInput::Start()
         return port.PortType() == PORT_TYPE::KEYBOARD;
       });
 
-    OpenKeyboard(it->CompatibleControllers().at(0).Controller());
+    OpenKeyboard(it->ActiveController().Controller());
   }
 
   // Open mouse
@@ -87,7 +89,7 @@ void CGameClientInput::Start()
         return port.PortType() == PORT_TYPE::MOUSE;
       });
 
-    OpenMouse(it->CompatibleControllers().at(0).Controller());
+    OpenMouse(it->ActiveController().Controller());
   }
 
   // Open joysticks
@@ -96,7 +98,7 @@ void CGameClientInput::Start()
   {
     if (port.PortType() == PORT_TYPE::CONTROLLER && !port.CompatibleControllers().empty())
     {
-      ControllerPtr controller = port.CompatibleControllers().at(0).Controller();
+      ControllerPtr controller = port.ActiveController().Controller();
       OpenJoystick(port.Address(), controller);
     }
   }
@@ -108,6 +110,8 @@ void CGameClientInput::Start()
 void CGameClientInput::Deinitialize()
 {
   Stop();
+
+  m_controllers.Clear();
 }
 
 void CGameClientInput::Stop()
@@ -168,6 +172,15 @@ void CGameClientInput::LoadTopology()
 
   CGameClientTopology topology(std::move(hardwarePorts));
   m_controllers = topology.GetControllerTree();
+}
+
+void CGameClientInput::ActivateControllers(CControllerHub &hub)
+{
+  for (auto &port : hub.Ports())
+  {
+    port.SetActiveController(0);
+    ActivateControllers(port.ActiveController().Hub());
+  }
 }
 
 bool CGameClientInput::SupportsKeyboard() const
--- xbmc/games/controllers/types/ControllerTree.cpp
+++ xbmc/games/controllers/types/ControllerTree.cpp
@@ -22,6 +22,7 @@
 #include "games/controllers/Controller.h"
 #include "games/controllers/ControllerTopology.h"
 
+#include <algorithm>
 #include <utility>
 
 using namespace KODI;
@@ -46,6 +47,13 @@ CControllerNode &CControllerNode::operator=(const CControllerNode &rhs)
   }
 
   return *this;
+}
+
+void CControllerNode::Clear()
+{
+  m_controller.reset();
+  m_address.clear();
+  m_hub.reset(new CControllerHub);
 }
 
 void CControllerNode::SetController(ControllerPtr controller)
@@ -126,6 +134,16 @@ const CControllerNode &CControllerPortNode::ActiveController() const
     return m_controllers[m_active];
 
   static const CControllerNode invalid{};
+  return invalid;
+}
+
+CControllerNode &CControllerPortNode::ActiveController()
+{
+  if (m_bConnected && m_active < m_controllers.size())
+    return m_controllers[m_active];
+
+  static CControllerNode invalid;
+  invalid.Clear();
   return invalid;
 }
 
@@ -212,6 +230,11 @@ CControllerHub &CControllerHub::operator=(const CControllerHub &rhs)
   }
 
   return *this;
+}
+
+void CControllerHub::Clear()
+{
+  m_ports.clear();
 }
 
 void CControllerHub::SetPorts(ControllerPortVec ports)
